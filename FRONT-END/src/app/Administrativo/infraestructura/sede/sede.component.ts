67d16f373bbae481623347d8c530babeee080118
Galo Pichucho <38517322+gjpichucho@users.noreply.github.com>
2020-09-14 04:55:19 -0500

    sede list

--- FRONT-END/src/app/Administrativo/infraestructura/sede/sede.component.ts
+++ FRONT-END/src/app/Administrativo/infraestructura/sede/sede.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Sede } from 'src/app/Models/infraestructura/sede';
 import { SedeService } from 'src/app/Services/Infra/Sede/sede.service';
+import { MenuItem } from 'primeng/api';
+
 
 @Component({
   selector: 'app-sede',
@@ -9,20 +11,59 @@ import { SedeService } from 'src/app/Services/Infra/Sede/sede.service';
 })
 export class SedeComponent implements OnInit {
 
+  cols: any[];
   sede: Sede[];
+  items: MenuItem[];
+  displaySaveDialog : boolean =false;
+  submitted: boolean;
+
   constructor(private sedeService: SedeService) { }
+
+  showSaveDialog(): void {
+    this.displaySaveDialog=true;
+  }
+  hideDialog() {
+    this.displaySaveDialog = false;
+    this.submitted = false;
+}
+  guardarSede(){
+    console.log("guardado");
+    
+  }
 
   ngOnInit(): void {
     this.sedeService.getAll().subscribe(data => {
       this.sede = data;
       console.log(this.sede);
-      console.log(data);
-      
+
 
     }, error => {
       console.log(error);
 
     })
+
+    this.cols = [
+      { field: 'COD_SEDE', header: 'Codigo' },
+      { field: 'NOMBRE', header: 'Nombre' },
+      { field: 'DIRECCION', header: 'Dirección' },
+      { field: 'TELEFONO', header: 'Telefono' },
+      { field: 'CODIGO_POSTAL', header: 'Codigo Postal' }
+    ];
+
+    this.items = [
+      {
+        label: "Nuevo",
+        icon: "pi pi-fw pi-plus",
+        command: () => this.showSaveDialog()
+      },
+      {
+        label: 'Editar',
+        icon: 'pi pi-fw pi-pencil',
+
+      }
+    ]
+
   }
+
 
 }
--- FRONT-END/src/app/Models/infraestructura/sede.ts
+++ FRONT-END/src/app/Models/infraestructura/sede.ts
@@ -1,8 +1,14 @@
 export class Sede{
-    cod_sede: string;
-    nombre: string;
-    direccion: string;
-    telefono: string;
-    codigo_postal: string;
+    // cod_sede: string;
+    // nombre: string;
+    // direccion: string;
+    // telefono: string;
+    // codigo_postal: string;
+
+    COD_SEDE: string;
+    NOMBRE: string;
+    DIRECCION: string;
+    TELEFONO: string;
+    CODIGO_POSTAL: string;
 
 }
