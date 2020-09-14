43b3562ce8a2f0f1a4d80afe50e3f1223869191f
Galo Pichucho <38517322+gjpichucho@users.noreply.github.com>
2020-09-13 22:39:11 -0500

    template angular

--- FRONT-END/README.md
+++ FRONT-END/README.md
@@ -1,6 +1,6 @@
-# FRONTEND
+# Prestige
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 10.0.8.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 8.1.2.
 
 ## Development server
 
--- FRONT-END/src/app/app.component.spec.ts
+++ FRONT-END/src/app/app.component.spec.ts
@@ -1,35 +1,36 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import { AppTopBarComponent } from './app.topbar.component';
+import { AppActionBarComponent } from './app.actionbar.component';
+import { AppConfigComponent } from './app.config.component';
+import { BreadcrumbService } from './breadcrumb.service';
+import { AppFooterComponent } from './app.footer.component';
+import { AppMenuComponent } from './app.menu.component';
+import { ScrollPanelModule } from 'primeng/scrollpanel';
+import { TabViewModule } from 'primeng/tabview';
+import { AccordionModule } from 'primeng/accordion';
+import { PanelModule } from 'primeng/panel';
 
 describe('AppComponent', () => {
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
-  }));
-
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'FRONT-END'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('FRONT-END');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('FRONT-END app is running!');
-  });
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            imports: [NoopAnimationsModule, RouterTestingModule, ScrollPanelModule, AccordionModule, PanelModule, TabViewModule],
+            declarations: [
+                AppComponent,
+                AppTopBarComponent,
+                AppActionBarComponent,
+                AppConfigComponent,
+                AppMenuComponent,
+                AppFooterComponent
+            ],
+            providers: [BreadcrumbService]
+        }).compileComponents();
+    }));
+    it('should create the app', async(() => {
+        const fixture = TestBed.createComponent(AppComponent);
+        const app = fixture.debugElement.componentInstance;
+        expect(app).toBeTruthy();
+    }));
 });
