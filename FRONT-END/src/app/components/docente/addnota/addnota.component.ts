45be1e00b335987d0ddb46a668383655755f8ca5
ExperimentStudent <56828277+ExperimentStudent@users.noreply.github.com>
2020-09-15 15:50:23 -0500

    nota

--- FRONT-END/src/app/components/docente/addnota/addnota.component.ts
+++ FRONT-END/src/app/components/docente/addnota/addnota.component.ts
@@ -8,25 +8,44 @@ import { NotaService } from "../../../Services/nota/nota.service";
   styleUrls: ['./addnota.component.css']
 })
 export class AddnotaComponent implements OnInit {
+  user = {
+    APELLIDO: '',
+    CEDULA: '',
+    CLAVE: '',
+    COD_PERSONA: '',
+    COD_PERSONA_REPRESENTANTE: '',
+    COD_ROL: '',
+    COD_USUARIO: '',
+    CORREO: '',
+    CORREO_PERSONAL: '',
+    DIRECCION: '',
+    ESTADO: '',
+    FECHA_NACIMIENTO: '',
+    GENERO: '',
+    INTENTOS_FALLIDOS: '',
+    NOMBRE: '',
+    NOMBRE_USUARIO: '',
+    TELEFONO: '',
+    ULT_FECHA_INGRESO: ''
+  }
 
   constructor(private router: Router, private notaService: NotaService) { }
 
   ngOnInit(): void {
+    this.user = JSON.parse(localStorage.getItem('user'));
+    this.listNivel();
   }
 
-  listNivel(cod_docente){
-    this.notaService.listarNivel(cod_docente).subscribe(data => {
+  listNivel(){
+    this.notaService.listarNivel(this.user.COD_PERSONA).subscribe(data => {
       //this.sede = data;
       console.log(data);
-
       // let sedes: Sede[] = [];
       //   for (let i = 0; i < data.length; i++) {
       //     let sede = data[i] as Sede;
       //     sedes.push(sede);
       //   }
       //   this.sede = sedes;
-
-
     }, error => {
       console.log(error);
 
@@ -34,15 +53,30 @@ export class AddnotaComponent implements OnInit {
   }
 
   listMaterias(){
+    this.notaService.listarAsignatura().subscribe(data => {
+      console.log(data);
+    }, error => {
+      console.log(error);
 
+    })
   }
 
   listParalelo(){
+    this.notaService.listarParalelo().subscribe(data => {
+      console.log(data);
+    }, error => {
+      console.log(error);
 
+    })
   }
 
   listEstudiantes(){
+    this.notaService.listarEstudiante().subscribe(data => {
+      console.log(data);
+    }, error => {
+      console.log(error);
 
+    })
   }
 
 }
