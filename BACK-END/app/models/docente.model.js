4fab5ab0b221a47e3987f492243d87e5c9192f83
ExperimentStudent <56828277+ExperimentStudent@users.noreply.github.com>
2020-09-15 20:04:22 -0500

    nota

--- BACK-END/app/models/docente.model.js
+++ BACK-END/app/models/docente.model.js
@@ -95,7 +95,8 @@ Tarea_Asignatura.listarMaterias = (cod_docente, cod_paralelo, result) => {
     sql.query(`SELECT a.NOMBRE, a.COD_ASIGNATURA FROM ASIGNATURA a
         INNER JOIN ASIGNATURA_PERIODO ap ON ap.COD_ASIGNATURA = a.COD_ASIGNATURA AND ap.COD_QUIMESTRE=1
         INNER JOIN ALUMNO_ASIGNATURA_PERIODO sp WHERE sp.COD_ASIGNATURA = ap.COD_ASIGNATURA AND sp.COD_PARALELO = ap.COD_PARALELO
-        AND sp.COD_DOCENTE = ${cod_docente} AND sp.COD_PARALELO = '${cod_paralelo}' AND sp.COD_QUIMESTRE=1;`, (err, res) => {
+        AND sp.COD_DOCENTE = ${cod_docente} AND sp.COD_PARALELO = '${cod_paralelo}' AND sp.COD_QUIMESTRE=1 
+        GROUP BY sp.COD_ASIGNATURA;`, (err, res) => {
         if (err) {
             console.log("error: ", err);
             result("hola" + err, null);
